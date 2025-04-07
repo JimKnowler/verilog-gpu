6d1abf774f91b57ed47fb662384fe3ceadf50ee0
Jim Knowler <jim.knowler2@gmail.com>
2025-04-07 15:30:08 +0100

    FixedPoint: separate fixtures for ToFixedPoint and FromFixedPoint

--- test/FromFixedPoint.test.cpp
+++ test/FromFixedPoint.test.cpp
@@ -3,32 +3,6 @@
 
 #include "FixedPoint.h"
 
-TEST(ToFixedPoint, ShouldSupportZero)
-{
-    EXPECT_EQ(0, ToFixedPoint(0.0f));
-}
-
-TEST(ToFixedPoint, ShouldSupportOne)
-{ 
-    EXPECT_EQ(1 << 8, ToFixedPoint(1.0f));
-}
-
-TEST(ToFixedPoint, ShouldSupportHalf)
-{ 
-    EXPECT_EQ(1 << 7, ToFixedPoint(0.5f));
-}
-
-TEST(ToFixedPoint, ShouldSupportQuarter)
-{ 
-    EXPECT_EQ(1 << 6, ToFixedPoint(0.25f));
-}
-
-TEST(ToFixedPoint, ShouldSupportPi)
-{
-    EXPECT_EQ(803, ToFixedPoint(3.14f));
-}
-
-
 TEST(FromFixedPoint, ShouldSupportZero)
 {
     EXPECT_FLOAT_EQ(0.0f, FromFixedPoint(0));
