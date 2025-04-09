2377092908016f508cf2687c2c021aec675d6957
Jim Knowler <jim.knowler2@gmail.com>
2025-04-09 10:43:50 +0100

    ClockedTestBench: add Reset()

--- test/ClockedTestBench.h
+++ test/ClockedTestBench.h
@@ -8,14 +8,28 @@ public:
     ClockedTestBench()
     {
         Module.i_clock = 0;
-
+        Module.i_reset_n = 1;
     }
 
+    /**
+     * @brief Tick the clock input on the module
+     */
     void Tick() {
         Module.i_clock = 1;
         Eval();
 
         Module.i_clock = 0;
         Eval();
+    }
+
+    /**
+     * @brief Reset the module
+     */
+    void Reset() {
+        Module.i_reset_n = 0;
+
+        Tick();
+
+        Module.i_reset_n = 1;
     }
 };
