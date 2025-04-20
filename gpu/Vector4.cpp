f2af9364d886f10af16d3d02cfaf7f7b614c9104
Jim Knowler <jim.knowler2@gmail.com>
2025-04-20 16:19:59 +0100

    Vector4: add unary '-' operator

--- gpu/Vector4.cpp
+++ gpu/Vector4.cpp
@@ -71,6 +71,16 @@ FVector4 FVector4::operator-(const FVector4 &Other) const
     return FVector4(X - Other.X, Y - Other.Y, Z - Other.Z, W - Other.W);
 }
 
+FVector4 FVector4::operator-() const
+{
+    return FVector4(
+        -X, 
+        -Y,
+        -Z,
+        -W
+    );
+}
+
 FVector4 FVector4::operator*(float Scalar) const
 {
     return FVector4(X * Scalar, Y * Scalar, Z * Scalar, W * Scalar);
